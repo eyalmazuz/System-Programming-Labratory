5b5c49a02f07e4ddeab155ad2194b177de299012
eyalmazuz <mazuz.eyal@gmail.com>
2018-11-25 13:04:09 +0200

    Added Future tests

--- Assignment-2/src/test/bgu/spl/mics/FutureTest.java
+++ Assignment-2/src/test/bgu/spl/mics/FutureTest.java
@@ -4,6 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.junit.Assert.*;
 
 public class FutureTest {
@@ -31,14 +34,51 @@ public class FutureTest {
 
     @Test
     public void testResolve() {
+        int test = 5;
+        future.resolve(test);
+        int get = future.get();
+        assertEquals(test, get);
 
     }
 
     @Test
-    public void isDone() {
+    public void testResolve2(){
+        AtomicInteger test = new AtomicInteger(0);
+        Thread t1= new Thread(()->{
+            test.set(future.get());
+        });
+        Thread t = new Thread(()->{
+            future.resolve(5);
+        });
+        t.start();
+        t1.start();
+        try {
+            t.join();
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertEquals(5, test.get());
+    }
+
+
+    @Test
+    public void isDoneBeforeResolve() {
+        assertEquals(false, future.isDone());
     }
 
     @Test
-    public void get1() {
+    public void isDoneAfterResolve() {
+        int test = 5;
+        future.resolve(test);
+        assertEquals(true, future.isDone());
     }
+
+    @Test
+    public void testTestResolve2(){
+        for (int i = 0; i < 100; i++) {
+            testResolve2();
+        }
+    }
+
 }
