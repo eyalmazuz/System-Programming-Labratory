4a7f41e69789fcfe28c2fd0fb928753b38380f98
eyal <mazuz.eyal@gmail.com>
2018-12-23 15:10:00 +0200

    ddad

---
+++ Assignment-3/Client/include/messageEncoder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by eyal on 12/23/18.
+//
+
+#include "messageEncoder.h"
---
+++ Assignment-3/Client/include/messageEncoder.h
@@ -0,0 +1,16 @@
+//
+// Created by eyal on 12/23/18.
+//
+
+#ifndef CLIENT_MESSAGEENCODER_H
+#define CLIENT_MESSAGEENCODER_H
+
+
+class messageEncoder {
+
+    public:
+    
+};
+
+
+#endif //CLIENT_MESSAGEENCODER_H
